65720aa442eab60ad513f21d6583fac2f5893ad4
JaderTS <jader-fernandes@live.com>
2016-05-09 23:56:47 -0300

    working with abstract classes

--- Labs/Labs/Lab_6_Abstract_Classes.cs
+++ Labs/Labs/Lab_6_Abstract_Classes.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Labs.AbstractClasses;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,18 @@ namespace Labs
     {
         static void Main(string[] args)
         {
+            ContaCorrente conta1 = new ContaCorrente("Fulano");
+            DateTime t = new DateTime(2009, 10, 21);
+            ContaPoupanca conta2 = new ContaPoupanca(0.5M, t, "Beltrano");
 
+            Console.WriteLine("Conta criada: " + conta1);
+            Console.WriteLine("Conta criada: " + conta2);
+
+            conta1.Depositar(100);
+            conta2.Depositar(200);
+
+            Console.WriteLine(conta1.Id + " - R$ " + conta1.Saldo);
+            Console.WriteLine(conta2.Id + " - R$ " + conta2.Saldo);
         }
     }
 }
