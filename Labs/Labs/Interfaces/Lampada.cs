9127d47e660e29255fd1652600d581005ead737a
JaderTS <jader-fernandes@live.com>
2016-05-16 00:27:00 -0300

    working with interfaces

---
+++ Labs/Labs/Interfaces/Lampada.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labs.Interfaces
+{
+    public class Lampada : IEstadoBinario
+    {
+        private bool ligado;
+        private int voltagem;
+        private int potencia;
+
+        public Lampada(int v, int p)
+        {
+            ligado = false;
+            voltagem = v;
+            potencia = p;
+        }
+
+        public int Voltagem
+        {
+            get { return voltagem; }
+        }
+
+        public int Potencia
+        {
+            get { return potencia; }
+        }
+
+        public void Ligar()
+        {
+            ligado = true;
+        }
+
+        public void Desligar()
+        {
+            ligado = false;
+        }
+
+        public EstadoBinario Estado
+        {
+            get
+            {
+                if (ligado) return EstadoBinario.Ligado;
+                else return EstadoBinario.Desligado;
+            }
+        }
+
+        public override string ToString() { return "Lampada: " + potencia + " W"; }
+    }
+}
